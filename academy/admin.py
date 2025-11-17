f13df4bc0ecd897989f155a4f72bd2826812ce21
Ajay Krishna Devadasan <54180151+Ajay998@users.noreply.github.com>
2025-11-17 20:24:13 +0530

    Add Course model and admin configuration with media settings

--- academy/admin.py
+++ academy/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from . models import *
 # Register your models here.
+
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ["id","course_name","description","duration","course_image"]
+    list_filter = ["course_name"]
+    search_fields = ['course_name']
+    list_editable = ["duration"]
+    list_display_links = ["course_name"]
+    ordering = ["-id"]
+
+
+admin.site.register(Course,CourseAdmin)
--- academy/models.py
+++ academy/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Course(models.Model):
+    course_name = models.CharField(max_length=50)
+    description = models.TextField()
+    duration = models.IntegerField()
+    course_image = models.ImageField(upload_to="course/", blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.course_name}"
