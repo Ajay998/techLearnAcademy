5ea49eb3689ee0187122c893c6823de8defdbb1e
Ajay krishna <krishnaajay998@gmail.com>
2025-11-18 14:46:14 +0530

    Add home page template and views functionality

--- academy/views.py
+++ academy/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from .models import Course, Trainer, Student
 # Create your views here.
+
+def home(request):
+    total_course = Course.objects.all().count()
+    total_trainer = Trainer.objects.all().count()
+    total_student = Student.objects.all().count()
+    context = {
+        "total_course" : total_course,
+        "total_trainer" : total_trainer,
+        "total_student" : total_student
+    }
+    return render(request,"homePage.html",context)
