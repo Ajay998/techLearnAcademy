95aed1bf674b171086616bfca9fb17832de36834
Ajay krishna <krishnaajay998@gmail.com>
2025-11-26 18:14:24 +0530

    Add course management features including detail view, edit, and delete functionality

--- academy/urls.py
+++ academy/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     path('academy/course/', views.course,name='course'),
     path('academy/trainer/', views.trainer, name='trainer'),
     path('academy/student/', views.student, name='student'),
+
+    path('academy/course/<int:id>',views.course_detail, name= "course_detail"),
+    path('academy/course/edit/<int:id>',views.edit_course, name= "edit_course"),
+    path('academy/course/delete/<int:id>',views.delete_course, name= "delete_course"),
 ]
