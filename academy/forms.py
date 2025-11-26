a58deb1a8c8d8f9f23b9e62858d34c3cba7cfbb0
Ajay krishna <krishnaajay998@gmail.com>
2025-11-26 20:21:24 +0530

    Add trainer management features including detail view, edit, and delete functionality

--- academy/forms.py
+++ academy/forms.py
@@ -1,8 +1,13 @@
 from django.forms import ModelForm
-from .models import Course
+from .models import Course, Trainer
 
 
 class CourseForm(ModelForm):
     class Meta:
         model = Course
+        fields = '__all__'
+
+class TrainerForm(ModelForm):
+    class Meta:
+        model = Trainer
         fields = '__all__'
